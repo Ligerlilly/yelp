cde95f357c6e6ca467a94baba3c17286f3352860
Ryan Broughan & Jason Lane <student@epicodus.com>
2015-08-17 14:54:48 -0700

    Add all, save, and equality methods to Restaurant

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,4 +8,26 @@ class Restaurant
 
   end
 
+  def self.all
+    returned_restaurants = DB.exec('SELECT * FROM restaurants;')
+    restaurants = []
+    returned_restaurants.each do |rest|
+      name = rest['name']
+      location = rest['location']
+      phone = rest['phone']
+      restaurants.push(Restaurant.new({ name: name, location: location, phone: phone }))
+    end
+    restaurants
+  end
+
+  def save
+    DB.exec("INSERT INTO restaurants (name, location, phone) VALUES ('#{@name}', '#{@location}', '#{@phone}');")
+  end
+
+  def ==(another_rest)
+    if self.name == another_rest.name && self.location == another_rest.location && self.phone == another_rest.phone
+      true
+    end
+  end
+
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,11 +2,41 @@ require 'spec_helper'
 
 describe Restaurant do
   before do
-    @rest = Restaurant.new({ name: 'Burger King', location: '1111 near you', phone: '55555555555' })
+    @rest = Restaurant.new({ name: 'Burger King', location: '1111 near you', phone: '5555555555' })
   end
+
   describe '#name' do
     it 'returns the name of the restaurant' do
       expect(@rest.name).to eq 'Burger King'
     end
   end
+
+  describe '#id' do
+    it 'returns the id of the restaurant' do
+      expect(@rest.id).to eq 1
+    end
+  end
+
+  describe '.all' do
+    it 'returns empty by default' do
+      expect(Restaurant.all).to eq ([])
+    end
+  end
+
+  describe '#save' do
+    it 'returns an array containing self' do
+      @rest.save
+      expect(Restaurant.all).to eq [@rest]
+    end
+  end
+
+  describe '#==' do
+    it 'returns true if the objects have the same attributes' do
+      @rest2 = Restaurant.new({ name: 'Burger King', location: '1111 near you', phone: '5555555555' })
+      expect(@rest2==@rest).to eq true
+    end
+  end
+
+  
+
 end
